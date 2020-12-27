7c00b7f16ccfcca6b369cbb0904e423e9f4d931a
Harry Stead <harrystead9@gmail.com>
2020-12-27 11:34:36 +0000

    declare variables in script

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+const auth = "563492ad6f917000010000018bf172d1d1c74e8d8fc7a818431e8655"; 
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const form = document.querySelector(".search-form");
+let searchValue;
+const more = document.querySelector(".more");
+let page = 1;
+let fetchLink;
+let currentSearch;
